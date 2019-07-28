46919fd1f77d3a5df2389966fb90508a880a9246
Javier Murillo <murillojh@gmail.com>
2019-07-28 10:46:04 -0400

    Record scaffold ref artist & ref user

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_144407) do
+ActiveRecord::Schema.define(version: 2019_07_28_144518) do
 
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "records", force: :cascade do |t|
+    t.string "title"
+    t.string "year"
+    t.integer "artist_id", null: false
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["artist_id"], name: "index_records_on_artist_id"
+    t.index ["user_id"], name: "index_records_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,4 +36,6 @@ ActiveRecord::Schema.define(version: 2019_07_28_144407) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "records", "artists"
+  add_foreign_key "records", "users"
 end
