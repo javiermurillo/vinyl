88ff5dd6062b2723fa0c37b9611c857e1c531ddb
Javier Murillo <murillojh@gmail.com>
2019-07-28 10:44:58 -0400

    Artist scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_28_144134) do
+ActiveRecord::Schema.define(version: 2019_07_28_144407) do
+
+  create_table "artists", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
